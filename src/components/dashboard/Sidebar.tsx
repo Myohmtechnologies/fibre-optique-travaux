a24f84b3328eb92f2a1f3319b6264c890dca5374
Riadh Mahieddine <riadhmahieddine@macbook-pro-de-riadh.home>
2025-03-21 11:15:26 +0100

    revieuws schema markup update1

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -70,7 +70,7 @@ export default function Sidebar() {
       href: '/dashboard/realizations',
       active: pathname === '/dashboard/realizations'
     },
-    {
+    {    
       name: 'Paramètres',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
